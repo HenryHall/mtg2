3a9c2a0f736e38ebbc3ce606de1c568077167039
HenryHall <mnboogie@yahoo.com>
2016-06-30 18:41:56 -0500

    small/medium demo

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use( bodyParser.json({limit: '50mb'}) );
+// app.use(bodyParser.urlencoded({extended: false}));
 var path = require('path');
 var mongoose = require('mongoose');
 mongoose.connect('localhost:27017/mtgApp');
@@ -11,19 +12,9 @@ var mtgSchema = new  mongoose.Schema({
     code: String,
     releaseDate: String,
     cards:{ type : Array , "default" : [] }
-      // [{
-      //   cmc: Number,
-      //   colors: String,
-      //   flavor: String,
-      //   manaCost: String,
-      //   multiverseid: Number,
-      //   name: String,
-      //   type: String,
-      //   types: Array
-      // }]
   });
 
-  var cardlist = mongoose.model( 'cardlist', mtgSchema );
+  var cardlists = mongoose.model( 'cardlists', mtgSchema );
 
   app.use( express.static( 'public' ) );
 
@@ -35,11 +26,28 @@ var mtgSchema = new  mongoose.Schema({
     res.sendFile( path.resolve( 'views/index.html' ) );
   });
 
-  app.get( '/getCard', function(req, res){
-    console.dir(req.body);
-    console.log("Trying to find: " + req.body);
-    // var query = cardlist.findOne({"name": "Shadows over Innistrad"});
-    res.send("done with: " + req.body.name);
+  app.post( '/getCard', function(req, res){
+    console.log("Trying to find: " + req.body.name);
+    cardKey = req.body.name.toLowerCase();
+    // var query = cardlists.findOne({name: "Limited Edition Alpha"});
+    // cardlists.findOne({name: "Shadows Over Innistrad"}).then(function(data){res.send(data)});
+    // cardlists.findOne({name: "Ice Age"}).then(function(data){res.send(data)});
+    cardlists.find().then(function(setList){
+      console.log("Got " + setList.length + " sets.");
+      for(var i=0; i<setList.length; i++){
+        console.log("Looking in set: " + setList[i].name);
+        for(var j=0; j<setList[i].cards.length; j++){
+          console.log("Looking at: " + setList[i].cards[j].name);
+          if(setList[i].cards[j].name.toLowerCase() == cardKey){
+            console.log("Found the requested card!");
+            res.send(setList[i].cards[j]);
+          }
+        }
+      }
+    }).then(function(){res.send("No Card Found!")});
+
+
+    // res.send("done with: " + req.body.name);
   });
 
   app.post('/createData', function(req, res){
@@ -47,7 +55,7 @@ var mtgSchema = new  mongoose.Schema({
 
     var newCard = req.body;
 
-    var newRecord = cardlist(newCard);
+    var newRecord = cardlists(newCard);
     newRecord.save();
     console.log(newRecord.name + " newRecord");
     res.send("db Created");
